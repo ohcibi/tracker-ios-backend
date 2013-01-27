caa4b912118b325c4faf7ef49b60b50e6f51b7d4
Tobias Witt <tobias.witt@hhu.de>
2013-01-27 23:36:59 +0100

    Change emails to use gravatar enabled ones.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
-User.create name: "Tobias Witt", email: "tobias.witt@hhu.de", password: "123456", password_confirmation: "123456"
-User.create name: "Michael Schöttner", email: "michael.schoettner@hhu.de", password: "123456", password_confirmation: "123456"
+User.create name: "Tobias Witt", email: "ich@dwgadf.de", password: "123456", password_confirmation: "123456"
+User.create name: "Michael Schöttner", email: "example@railstutorial.org", password: "123456", password_confirmation: "123456"
 User.create name: "Angela Rennwanz", email: "rennwanz@cs.uni-duesseldorf.de", password: "123456", password_confirmation: "123456"
 User.create name: "André Ippisch", email: "andre.ippisch@hhu.de", password: "123456", password_confirmation: "123456"
 User.create name: "Tobias Krauthoff", email: "tobias.krauthoff@hhu.de", password: "123456", password_confirmation: "123456"
